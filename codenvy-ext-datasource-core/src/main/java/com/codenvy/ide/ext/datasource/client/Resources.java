16ff565b9c2b0b2860477bd3255fe182da4e2a18
Mickaël Leduque <mickael.leduque@serli.com>
2014-01-13 09:31:27 +0100

    remove an unused import

--- codenvy-ext-datasource-core/src/main/java/com/codenvy/ide/ext/datasource/client/Resources.java
+++ codenvy-ext-datasource-core/src/main/java/com/codenvy/ide/ext/datasource/client/Resources.java
@@ -2,7 +2,6 @@ package com.codenvy.ide.ext.datasource.client;
 
 import com.google.gwt.resources.client.ClientBundle;
 import com.google.gwt.resources.client.ImageResource;
-import com.google.gwt.resources.client.ClientBundle.Source;
 
 public interface Resources extends ClientBundle{
 
--- codenvy-ext-datasource-core/src/main/java/com/codenvy/ide/ext/datasource/server/DatasourceService.java
+++ codenvy-ext-datasource-core/src/main/java/com/codenvy/ide/ext/datasource/server/DatasourceService.java
@@ -22,7 +22,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 import javax.sql.DataSource;
-import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
