6dc312ec1860f6d5a6ffd955b435afaff1515fbd
Mickaël Leduque <mickael.leduque@serli.com>
2014-01-13 13:52:43 +0100

    Add missing javadoc

--- codenvy-ext-datasource-core/src/main/java/com/codenvy/ide/ext/datasource/client/Resources.java
+++ codenvy-ext-datasource-core/src/main/java/com/codenvy/ide/ext/datasource/client/Resources.java
@@ -3,8 +3,16 @@ package com.codenvy.ide.ext.datasource.client;
 import com.google.gwt.resources.client.ClientBundle;
 import com.google.gwt.resources.client.ImageResource;
 
+/**
+ * Client bundle interface for datasource plugin resources.
+ */
 public interface Resources extends ClientBundle {
 
+    /**
+     * The PostgreSQL logo.
+     * 
+     * @return an image resource for the PostGreSQL logo.
+     */
     @Source("PostgreSQL.png")
     ImageResource getPostgreSqlLogo();
 }
