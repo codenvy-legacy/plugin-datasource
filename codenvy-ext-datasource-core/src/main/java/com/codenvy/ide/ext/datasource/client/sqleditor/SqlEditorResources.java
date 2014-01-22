0058419a82f01e42680d42c905dcbfafcdf1016d
Mickaël Leduque <mickael.leduque@serli.com>
2014-01-22 16:29:07 +0100

    Add ressources for Sql editor

---
+++ codenvy-ext-datasource-core/src/main/java/com/codenvy/ide/ext/datasource/client/sqleditor/SqlEditorResources.java
@@ -0,0 +1,14 @@
+package com.codenvy.ide.ext.datasource.client.sqleditor;
+
+import com.google.gwt.resources.client.ClientBundle;
+import com.google.gwt.resources.client.ImageResource;
+import com.google.gwt.resources.client.TextResource;
+
+public interface SqlEditorResources extends ClientBundle {
+
+    @Source("codemirror-parser-sql.js")
+    TextResource sqlParserJs();
+
+    @Source("sql-icon.png")
+    ImageResource sqlFile();
+}
