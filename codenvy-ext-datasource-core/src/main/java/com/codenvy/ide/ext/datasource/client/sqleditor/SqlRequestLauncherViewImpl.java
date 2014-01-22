9bae26e8da87113355f35914c05d0a2462c17d9a
Mickaël Leduque <mickael.leduque@serli.com>
2014-01-22 11:00:00 +0100

    Place control on sql request launcher view

--- codenvy-ext-datasource-core/src/main/java/com/codenvy/ide/ext/datasource/client/sqleditor/SqlRequestLauncherViewImpl.java
+++ codenvy-ext-datasource-core/src/main/java/com/codenvy/ide/ext/datasource/client/sqleditor/SqlRequestLauncherViewImpl.java
@@ -17,8 +17,16 @@
  */
 package com.codenvy.ide.ext.datasource.client.sqleditor;
 
+import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.user.client.ui.AcceptsOneWidget;
+import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.ListBox;
+import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.inject.Inject;
 
@@ -26,15 +34,45 @@ public class SqlRequestLauncherViewImpl extends Composite implements SqlRequestL
 
     private ActionDelegate actionDelegate;
 
+    @UiField
+    AcceptsOneWidget       editionZone;
+
+    @UiField
+    AcceptsOneWidget       resultZone;
+
+    @UiField
+    Label                  selectDatasourceLabel;
+
+    @UiField
+    ListBox                datasourceList;
+
+    @UiField
+    Label                  resultLimitLabel;
+
+    @UiField
+    TextBox                resultLimitInput;
+
+    @UiField
+    Button                 executeButton;
+
+
     @Inject
-    public SqlRequestLauncherViewImpl(final SqlRequestLauncherViewImplUiBinder uiBinder) {
+    public SqlRequestLauncherViewImpl(final SqlRequestLauncherViewImplUiBinder uiBinder,
+                                      final SqlRequestLauncherConstants constants) {
         initWidget(uiBinder.createAndBindUi(this));
+        selectDatasourceLabel.setText(constants.selectDatasourceLabel());
+        resultLimitLabel.setText(constants.resultLimitLabel());
     }
 
     /** {@inheritDoc} */
     @Override
     public void setDelegate(final ActionDelegate delegate) {
         this.actionDelegate = delegate;
+    }
+
+    @UiHandler("executeButton")
+    void handleExecuteClick(final ClickEvent e) {
+
     }
 
     interface SqlRequestLauncherViewImplUiBinder extends UiBinder<Widget, SqlRequestLauncherViewImpl> {
