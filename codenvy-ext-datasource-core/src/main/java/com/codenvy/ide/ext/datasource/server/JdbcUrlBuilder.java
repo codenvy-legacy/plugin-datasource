6d59f45b576b26005c97e38bda1b6dd0690a89bc
Mickaël Leduque <mickael.leduque@serli.com>
2014-01-24 10:46:28 +0100

    Add jdbcUrlBuilder with postgres implementation

---
+++ codenvy-ext-datasource-core/src/main/java/com/codenvy/ide/ext/datasource/server/JdbcUrlBuilder.java
@@ -0,0 +1,55 @@
+package com.codenvy.ide.ext.datasource.server;
+
+import java.text.MessageFormat;
+
+import com.codenvy.ide.ext.datasource.shared.DatabaseConfigurationDTO;
+import com.codenvy.ide.ext.datasource.shared.exception.DatabaseDefinitionException;
+
+public class JdbcUrlBuilder {
+    private static final String URL_TEMPLATE_POSTGRES = "jdbc:postgresql://{0}:{1}/{2}";
+
+    public String getJdbcUrl(final DatabaseConfigurationDTO configuration) throws DatabaseDefinitionException {
+        // Should we check and sanitize input values ?
+        if (configuration.getDatabaseType() == null) {
+            throw new DatabaseDefinitionException("Database type is null in " + configuration.toString());
+        }
+        switch (configuration.getDatabaseType()) {
+            case POSTGRES:
+                return getPostgresJdbcUrl(configuration);
+            case MYSQL:
+                return getMySQLJdbcUrl(configuration);
+            case ORACLE:
+                return getOracleJdbcUrl(configuration);
+            case MSSQL:
+                return getMSSQLJdbcUrl(configuration);
+            default:
+                throw new DatabaseDefinitionException("Unknown database type "
+                                                      + configuration.getDatabaseType()
+                                                      + " in "
+                                                      + configuration.toString());
+        }
+    }
+
+    private String getPostgresJdbcUrl(final DatabaseConfigurationDTO configuration) {
+        String url = MessageFormat.format(URL_TEMPLATE_POSTGRES,
+                                          configuration.getHostname(),
+                                          Integer.toString(configuration.getPort()),
+                                          configuration.getDatabaseName());
+        return url;
+    }
+
+    private String getMySQLJdbcUrl(final DatabaseConfigurationDTO configuration) {
+        // TODO implement
+        return null;
+    }
+
+    private String getOracleJdbcUrl(final DatabaseConfigurationDTO configuration) {
+        // TODO implement
+        return null;
+    }
+
+    private String getMSSQLJdbcUrl(final DatabaseConfigurationDTO configuration) {
+        // TODO implement
+        return null;
+    }
+}
