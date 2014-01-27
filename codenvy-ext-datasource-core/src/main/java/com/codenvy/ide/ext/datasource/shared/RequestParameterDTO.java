9356f06070033049088e04c96092e5e0f84e8b77
Mickaël Leduque <mickael.leduque@serli.com>
2014-01-27 18:30:01 +0100

    Add DTO for request parameter

---
+++ codenvy-ext-datasource-core/src/main/java/com/codenvy/ide/ext/datasource/shared/RequestParameterDTO.java
@@ -0,0 +1,25 @@
+package com.codenvy.ide.ext.datasource.shared;
+
+import com.codenvy.dto.shared.DTO;
+
+@DTO
+public interface RequestParameterDTO {
+
+    DatabaseConfigurationDTO getDatabase();
+
+    void setDatabase(DatabaseConfigurationDTO database);
+
+    RequestParameterDTO withDatabase(DatabaseConfigurationDTO database);
+
+    String getSqlRequest();
+
+    void setSqlRequest(String sqlRequest);
+
+    RequestParameterDTO withSqlRequest(String sqlRequest);
+
+    int getResultLimit();
+
+    void setResultLimit(int resultLimit);
+
+    RequestParameterDTO withResultLimit(int resultLimit);
+}
