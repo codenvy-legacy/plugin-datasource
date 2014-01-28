48bdd2d1f361bf6bac97ec5655504ed4a15dd075
Mickaël Leduque <mickael.leduque@serli.com>
2014-01-28 16:29:37 +0100

    Add constants interface for the explorer

---
+++ codenvy-ext-datasource-core/src/main/java/com/codenvy/ide/ext/datasource/client/explorer/DatasourceExplorerConstants.java
@@ -0,0 +1,9 @@
+package com.codenvy.ide.ext.datasource.client.explorer;
+
+import com.google.gwt.i18n.client.Constants;
+
+public interface DatasourceExplorerConstants extends Constants {
+
+    @DefaultStringValue("Explore")
+    String exploreButtonLabel();
+}
