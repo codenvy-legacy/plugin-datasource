d775031b043a65cf70c03da4fc4ae4cf49a772e4
Mickaël Leduque <mickael.leduque@serli.com>
2014-01-29 17:12:15 +0100

    Create custom textarea for the sql launcher

---
+++ codenvy-ext-datasource-core/src/main/java/com/codenvy/ide/ext/datasource/client/sqllauncher/SqlLauncherTextArea.java
@@ -0,0 +1,14 @@
+package com.codenvy.ide.ext.datasource.client.sqllauncher;
+
+import com.google.gwt.user.client.ui.TextArea;
+
+// temporary class
+public class SqlLauncherTextArea extends TextArea {
+
+    SqlLauncherTextArea(boolean readOnly) {
+        super();
+        setReadOnly(readOnly);
+        getElement().getStyle().setBackgroundColor("white");
+        getElement().getStyle().setProperty("font-family", "monospace");
+    }
+}
