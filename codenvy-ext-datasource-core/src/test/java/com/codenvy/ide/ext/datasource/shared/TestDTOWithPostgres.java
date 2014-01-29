34410464fb221d3374e7821de63aecea587920de
Sun Seng David TAN <sunix@sunix.org>
2014-01-29 17:12:23 +0100

    Cleaning up database dto generation tests

--- codenvy-ext-datasource-core/src/test/java/com/codenvy/ide/ext/datasource/shared/TestDTOWithPostgres.java
+++ codenvy-ext-datasource-core/src/test/java/com/codenvy/ide/ext/datasource/shared/TestDTOWithPostgres.java
@@ -29,29 +29,28 @@ import org.mockito.MockitoAnnotations;
 import com.codenvy.ide.ext.datasource.server.DatasourceService;
 import com.codenvy.ide.ext.datasource.server.JdbcUrlBuilder;
 
-public class TestDtoWithMysql {
-
+public class TestDTOWithPostgres {
     @Mock
     protected DatabaseConfigurationDTO databaseConfig;
 
     @Before
     public void init() {
         MockitoAnnotations.initMocks(this);
-        when(databaseConfig.getDatabaseType()).thenReturn(DatabaseType.MYSQL);
-        when(databaseConfig.getDatabaseName()).thenReturn("aucoffre_db");
+        when(databaseConfig.getDatabaseType()).thenReturn(DatabaseType.POSTGRES);
+        when(databaseConfig.getDatabaseName()).thenReturn("nuxeo");
         when(databaseConfig.getHostname()).thenReturn("localhost");
-        when(databaseConfig.getPort()).thenReturn(3306);
-        when(databaseConfig.getUsername()).thenReturn("root");
-        when(databaseConfig.getPassword()).thenReturn("selucreh");
+        when(databaseConfig.getPort()).thenReturn(5432);
+        when(databaseConfig.getUsername()).thenReturn("postgres");
+        when(databaseConfig.getPassword()).thenReturn("nuxeospirit");
     }
     
     @Ignore
     @Test
-    public void testMySQLDTOgeneration() throws Exception {
+    public void testPostgresDTOgeneration() throws Exception {
         DatasourceService dsService = new DatasourceService(new JdbcUrlBuilder());
         String json = dsService.getDatabase(databaseConfig);
         System.out.println(json);
         Assert.assertNotNull(json);
-        Assert.assertTrue(json.contains("\"databaseProductName\":\"MySQL\""));
+        Assert.assertTrue(json.contains("\"databaseProductName\":\"PostgreSQL\""));
     }
 }
