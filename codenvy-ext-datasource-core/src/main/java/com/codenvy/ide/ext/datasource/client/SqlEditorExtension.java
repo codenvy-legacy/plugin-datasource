d6f000ad6a06bb3be03f35ed3c4f5b62c64641a4
Mickaël Leduque <mickael.leduque@serli.com>
2014-02-03 16:57:29 +0100

    Register resource provider for SQL files

--- codenvy-ext-datasource-core/src/main/java/com/codenvy/ide/ext/datasource/client/SqlEditorExtension.java
+++ codenvy-ext-datasource-core/src/main/java/com/codenvy/ide/ext/datasource/client/SqlEditorExtension.java
@@ -19,12 +19,16 @@ package com.codenvy.ide.ext.datasource.client;
 
 import static com.google.gwt.core.client.ScriptInjector.TOP_WINDOW;
 
+import com.codenvy.ide.api.editor.EditorRegistry;
 import com.codenvy.ide.api.extension.Extension;
 import com.codenvy.ide.api.resources.FileType;
 import com.codenvy.ide.api.resources.ResourceProvider;
 import com.codenvy.ide.api.ui.action.ActionManager;
+import com.codenvy.ide.api.ui.wizard.newresource.NewResourceAgent;
 import com.codenvy.ide.api.ui.workspace.WorkspaceAgent;
+import com.codenvy.ide.ext.datasource.client.sqleditor.SqlEditorProvider;
 import com.codenvy.ide.ext.datasource.client.sqleditor.SqlEditorResources;
+import com.codenvy.ide.ext.datasource.client.sqleditor.SqlResourceProvider;
 import com.codenvy.ide.util.loging.Log;
 import com.google.gwt.core.client.ScriptInjector;
 import com.google.inject.Inject;
@@ -48,7 +52,11 @@ public class SqlEditorExtension {
     public SqlEditorExtension(final WorkspaceAgent workspaceAgent,
                               final ActionManager actionManager,
                               final SqlEditorResources sqlEditorResources,
-                              final ResourceProvider resourceProvider) {
+                              final ResourceProvider resourceProvider,
+                              final EditorRegistry editorRegistry,
+                              final SqlEditorProvider sqlEditorProvider,
+                              final NewResourceAgent newResourceAgent,
+                              final SqlResourceProvider sqlResourceProvider) {
 
         Log.info(SqlEditorExtension.class, "Initialization of SQL editor extension.");
         // inject sql parser
@@ -56,6 +64,8 @@ public class SqlEditorExtension {
 
         final FileType sqlFile = new FileType(sqlEditorResources.sqlFile(), GENERIC_SQL_MIME_TYPE, SQL_FILE_EXTENSION);
         resourceProvider.registerFileType(sqlFile);
+        editorRegistry.register(sqlFile, sqlEditorProvider);
+        newResourceAgent.register(sqlResourceProvider);
     }
 
 }
