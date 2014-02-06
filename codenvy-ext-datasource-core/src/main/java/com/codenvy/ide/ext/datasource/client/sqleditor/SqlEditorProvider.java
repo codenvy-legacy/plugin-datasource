829b35aeb369ac685f5b1884176084ef93fdcb24
Mickaël Leduque <mickael.leduque@serli.com>
2014-02-06 10:09:55 +0100

    Change actual return type of SQL editor provider

--- codenvy-ext-datasource-core/src/main/java/com/codenvy/ide/ext/datasource/client/sqleditor/SqlEditorProvider.java
+++ codenvy-ext-datasource-core/src/main/java/com/codenvy/ide/ext/datasource/client/sqleditor/SqlEditorProvider.java
@@ -2,9 +2,10 @@ package com.codenvy.ide.ext.datasource.client.sqleditor;
 
 import com.codenvy.ide.api.editor.CodenvyTextEditor;
 import com.codenvy.ide.api.editor.DocumentProvider;
-import com.codenvy.ide.api.editor.EditorPartPresenter;
 import com.codenvy.ide.api.editor.EditorProvider;
+import com.codenvy.ide.api.editor.TextEditorPartPresenter;
 import com.codenvy.ide.api.notification.NotificationManager;
+import com.codenvy.ide.util.loging.Log;
 import com.google.inject.Inject;
 import com.google.inject.Provider;
 
@@ -26,7 +27,8 @@ public class SqlEditorProvider implements EditorProvider {
     }
 
     @Override
-    public EditorPartPresenter getEditor() {
+    public TextEditorPartPresenter getEditor() {
+        Log.info(SqlEditorProvider.class, "New instance of SQL editor requested.");
         CodenvyTextEditor textEditor = editorProvider.get();
         textEditor.initialize(new SqlEditorConfiguration(), documentProvider, notificationManager);
         return textEditor;
