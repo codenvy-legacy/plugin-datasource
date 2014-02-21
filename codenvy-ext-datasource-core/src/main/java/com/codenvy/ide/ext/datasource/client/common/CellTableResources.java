e462ac854b237d6e581df9e805eae3afa28abea6
Mickaël Leduque <mickael.leduque@serli.com>
2014-02-21 16:52:21 +0100

    Add accessors to result headers styles

--- codenvy-ext-datasource-core/src/main/java/com/codenvy/ide/ext/datasource/client/common/CellTableResources.java
+++ codenvy-ext-datasource-core/src/main/java/com/codenvy/ide/ext/datasource/client/common/CellTableResources.java
@@ -5,6 +5,14 @@ import com.google.gwt.user.cellview.client.CellTable;
 public interface CellTableResources extends CellTable.Resources {
 
     public interface CellTableStyle extends CellTable.Style {
+
+        String infoHeader();
+
+        String queryReminder();
+
+        String csvButton();
+
+        String infoHeaderTitle();
     }
 
     @Source({"CellTable.css", "com/codenvy/ide/api/ui/style.css"})
