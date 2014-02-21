04470be6910b6410399a4091858cc177fc80262d
Mickaël Leduque <mickael.leduque@serli.com>
2014-02-21 11:55:49 +0100

    Create a css resource for celltables

---
+++ codenvy-ext-datasource-core/src/main/java/com/codenvy/ide/ext/datasource/client/common/CellTableResources.java
@@ -0,0 +1,12 @@
+package com.codenvy.ide.ext.datasource.client.common;
+
+import com.google.gwt.user.cellview.client.CellTable;
+
+public interface CellTableResources extends CellTable.Resources {
+
+    public interface CellTableStyle extends CellTable.Style {
+    }
+
+    @Source({"CellTable.css", "com/codenvy/ide/api/ui/style.css"})
+    CellTableStyle cellTableStyle();
+}
