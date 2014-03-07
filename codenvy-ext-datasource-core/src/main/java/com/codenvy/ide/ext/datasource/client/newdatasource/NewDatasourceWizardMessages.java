52c5effada66a0b472bb8653541773dc26d062ae
Mickaël Leduque <mickael.leduque@serli.com>
2014-03-07 10:45:31 +0100

    Add messages interface for the new datasource wizard

---
+++ codenvy-ext-datasource-core/src/main/java/com/codenvy/ide/ext/datasource/client/newdatasource/NewDatasourceWizardMessages.java
@@ -0,0 +1,9 @@
+package com.codenvy.ide.ext.datasource.client.newdatasource;
+
+import com.google.gwt.i18n.client.Messages;
+
+public interface NewDatasourceWizardMessages extends Messages {
+
+    @DefaultMessage("New Datasource")
+    String wizardTitle();
+}
