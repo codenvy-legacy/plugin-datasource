45517d67166b10e73de55185e5101dbe64c8e2f5
Mickaël Leduque <mickael.leduque@serli.com>
2014-03-10 14:26:23 +0100

    Add connector for nuoDB - #45 At the moment, only support a single broker

---
+++ codenvy-ext-datasource-core/src/main/java/com/codenvy/ide/ext/datasource/client/newdatasource/connector/nuodb/NuoDBDatasourceConnectorPage.java
@@ -0,0 +1,67 @@
+package com.codenvy.ide.ext.datasource.client.newdatasource.connector.nuodb;
+
+import com.codenvy.ide.api.notification.NotificationManager;
+import com.codenvy.ide.dto.DtoFactory;
+import com.codenvy.ide.ext.datasource.client.DatasourceClientService;
+import com.codenvy.ide.ext.datasource.client.DatasourceManager;
+import com.codenvy.ide.ext.datasource.client.DatasourceUiResources;
+import com.codenvy.ide.ext.datasource.client.newdatasource.NewDatasourceWizard;
+import com.codenvy.ide.ext.datasource.client.newdatasource.NewDatasourceWizardMessages;
+import com.codenvy.ide.ext.datasource.client.newdatasource.connector.AbstractNewDatasourceConnectorPage;
+import com.codenvy.ide.ext.datasource.client.newdatasource.connector.JdbcDatasourceConnectorView;
+import com.codenvy.ide.ext.datasource.shared.DatabaseConfigurationDTO;
+import com.codenvy.ide.ext.datasource.shared.DatabaseType;
+import com.google.gwt.user.client.ui.AcceptsOneWidget;
+import com.google.inject.Inject;
+import com.google.web.bindery.event.shared.EventBus;
+
+
+/**
+ * This connector page is using JTDS JDBC Driver to connect to MS SQLserver.
+ */
+public class NuoDBDatasourceConnectorPage extends AbstractNewDatasourceConnectorPage
+                                                                                    implements
+                                                                                    JdbcDatasourceConnectorView.ActionDelegate {
+
+    public static final String NUODB_DB_ID               = "nuodb";
+    private static final int   DEFAULT_PORT_NUODB_BROKER = 48004;
+
+    private final DtoFactory   dtoFactory;
+
+    @Inject
+    public NuoDBDatasourceConnectorPage(final JdbcDatasourceConnectorView view,
+                                        final NotificationManager notificationManager,
+                                        final DtoFactory dtoFactory,
+                                        final DatasourceManager datasourceManager,
+                                        final EventBus eventBus,
+                                        final DatasourceClientService service,
+                                        final DatasourceUiResources resources,
+                                        final NewDatasourceWizardMessages messages) {
+        super(view, "NuoDB", null, NUODB_DB_ID, datasourceManager, eventBus, service,
+              notificationManager, dtoFactory, messages);
+        this.dtoFactory = dtoFactory;
+    }
+
+
+    @Override
+    public void go(final AcceptsOneWidget container) {
+        container.setWidget(getView());
+        // set the default port
+        getView().setPort(DEFAULT_PORT_NUODB_BROKER);
+    }
+
+
+    protected DatabaseConfigurationDTO getConfiguredDatabase() {
+        String datasourceId = wizardContext.getData(NewDatasourceWizard.DATASOURCE_NAME);
+        DatabaseConfigurationDTO result =
+                                          dtoFactory.createDto(DatabaseConfigurationDTO.class)
+                                                    .withDatabaseName(getView().getDatabaseName())
+                                                    .withHostname(getView().getHostname()).withPort(getView().getPort())
+                                                    .withUsername(getView().getUsername())
+                                                    .withPassword(getView().getPassword())
+                                                    .withDatabaseType(DatabaseType.NUODB)
+                                                    .withDatasourceId(datasourceId);
+        return result;
+    }
+
+}
