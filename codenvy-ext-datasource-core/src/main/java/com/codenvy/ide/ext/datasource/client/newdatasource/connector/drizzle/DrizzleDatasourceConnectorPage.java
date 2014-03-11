daa43c4a810eed652dc91f6417b5550076f03dc9
Mickaël Leduque <mickael.leduque@serli.com>
2014-03-11 10:27:08 +0100

    Add a drizzle wizard connector (not used)

--- codenvy-ext-datasource-core/src/main/java/com/codenvy/ide/ext/datasource/client/newdatasource/connector/drizzle/DrizzleDatasourceConnectorPage.java
+++ codenvy-ext-datasource-core/src/main/java/com/codenvy/ide/ext/datasource/client/newdatasource/connector/drizzle/DrizzleDatasourceConnectorPage.java
@@ -6,21 +6,26 @@ import com.codenvy.ide.ext.datasource.client.DatasourceClientService;
 import com.codenvy.ide.ext.datasource.client.DatasourceManager;
 import com.codenvy.ide.ext.datasource.client.DatasourceUiResources;
 import com.codenvy.ide.ext.datasource.client.newdatasource.NewDatasourceWizardMessages;
-import com.codenvy.ide.ext.datasource.client.newdatasource.connector.AbstractNewDatasourceConnectorPage;
-import com.codenvy.ide.ext.datasource.client.newdatasource.connector.JdbcDatasourceConnectorView;
+import com.codenvy.ide.ext.datasource.client.newdatasource.connector.DefaultNewDatasourceConnectorPage;
+import com.codenvy.ide.ext.datasource.client.newdatasource.connector.DefaultNewDatasourceConnectorView;
 import com.codenvy.ide.ext.datasource.shared.DatabaseType;
 import com.google.inject.Inject;
 import com.google.web.bindery.event.shared.EventBus;
 
-public class DrizzleDatasourceConnectorPage extends AbstractNewDatasourceConnectorPage
-                                                                                      implements JdbcDatasourceConnectorView.ActionDelegate {
+/**
+ * Connector for the Drizzle database implementation.
+ * 
+ * @author "Mickaël Leduque"
+ */
+public class DrizzleDatasourceConnectorPage extends DefaultNewDatasourceConnectorPage {
 
     public static final String DRIZZLE_DB_ID        = "drizzle";
+
+    /** The default port for drizzle. */
     private static final int   DEFAULT_PORT_DRIZZLE = 4427;
 
-
     @Inject
-    public DrizzleDatasourceConnectorPage(final JdbcDatasourceConnectorView view,
+    public DrizzleDatasourceConnectorPage(final DefaultNewDatasourceConnectorView view,
                                           final NotificationManager notificationManager,
                                           final DtoFactory dtoFactory,
                                           final DatasourceManager datasourceManager,
@@ -29,18 +34,6 @@ public class DrizzleDatasourceConnectorPage extends AbstractNewDatasourceConnect
                                           final DatasourceUiResources resources,
                                           final NewDatasourceWizardMessages messages) {
         super(view, "Drizzle", null, DRIZZLE_DB_ID, datasourceManager, eventBus, service, notificationManager,
-              dtoFactory, messages);
+              dtoFactory, messages, DEFAULT_PORT_DRIZZLE, DatabaseType.DRIZZLE);
     }
-
-    @Override
-    public Integer getDefaultPort() {
-        return DEFAULT_PORT_DRIZZLE;
-    }
-
-    @Override
-    public DatabaseType getDatabaseType() {
-        return DatabaseType.DRIZZLE;
-    }
-
-
 }
