38e559dfd83500d5815c7cf18bc591fc73c7e377
Mickaël Leduque <mickael.leduque@serli.com>
2014-03-26 10:53:50 +0100

    Add missing copyright notices - #71

--- codenvy-ext-datasource-core/src/main/java/com/codenvy/ide/ext/datasource/client/common/HyperlinkCell.java
+++ codenvy-ext-datasource-core/src/main/java/com/codenvy/ide/ext/datasource/client/common/HyperlinkCell.java
@@ -1,9 +1,27 @@
+/*
+ * CODENVY CONFIDENTIAL
+ * __________________
+ *
+ * [2013] - [2014] Codenvy, S.A.
+ * All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Codenvy S.A. and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Codenvy S.A.
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Codenvy S.A..
+ */
 package com.codenvy.ide.ext.datasource.client.common;
 
 import com.google.gwt.cell.client.Cell;
 import com.google.gwt.cell.client.TextCell;
 import com.google.gwt.core.shared.GWT;
 import com.google.gwt.safehtml.client.SafeHtmlTemplates;
+import com.google.gwt.safehtml.client.SafeHtmlTemplates.Template;
 import com.google.gwt.safehtml.shared.SafeHtml;
 import com.google.gwt.safehtml.shared.SafeUri;
 import com.google.gwt.safehtml.shared.UriUtils;
