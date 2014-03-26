082978cfe995a72a51cf843425b528a4ca347e94
Mickaël Leduque <mickael.leduque@serli.com>
2014-03-26 09:52:59 +0100

    Add a CellTable resource dedicated to the datasource properties table

---
+++ codenvy-ext-datasource-core/src/main/java/com/codenvy/ide/ext/datasource/client/common/CellTableResourcesProperties.java
@@ -0,0 +1,12 @@
+package com.codenvy.ide.ext.datasource.client.common;
+
+import com.google.gwt.user.cellview.client.CellTable;
+
+public interface CellTableResourcesProperties extends CellTable.Resources {
+
+    interface CellTableStyle extends CellTable.Style {
+    }
+
+    @Source({"CellTable-properties.css", "com/codenvy/ide/api/ui/style.css"})
+    CellTableStyle cellTableStyle();
+}
