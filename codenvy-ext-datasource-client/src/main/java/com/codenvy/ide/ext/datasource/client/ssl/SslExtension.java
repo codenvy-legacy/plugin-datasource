7212eae3aca7350008901a0ffb1024307193727c
Mickaël Leduque <mickael.leduque@serli.com>
2014-03-31 15:44:21 +0200

    Fix copyright notices in SSL store section

--- codenvy-ext-datasource-client/src/main/java/com/codenvy/ide/ext/datasource/client/ssl/SslExtension.java
+++ codenvy-ext-datasource-client/src/main/java/com/codenvy/ide/ext/datasource/client/ssl/SslExtension.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2014 Codenvy, S.A.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.codenvy.ide.ext.datasource.client.ssl;
 
 import com.codenvy.ide.api.extension.Extension;
@@ -14,15 +29,15 @@ public class SslExtension {
     public SslExtension(SslKeyStoreClientService sslKeyStoreClientService) {
         Log.info(SslExtension.class, "Init SSL callback");
         sslKeyStoreClientService.init(new AsyncRequestCallback<Void>() {
-                @Override
-                public void onSuccess(Void result) {
-                    Log.info(SslExtension.class, "Succeeded SSL init");
-                }
+            @Override
+            public void onSuccess(Void result) {
+                Log.info(SslExtension.class, "Succeeded SSL init");
+            }
 
-                @Override
-                public void onFailure(Throwable exception) {
-                    Log.error(SslExtension.class, "Failed tnit SSL callback", exception);
-                }
-            });
+            @Override
+            public void onFailure(Throwable exception) {
+                Log.error(SslExtension.class, "Failed tnit SSL callback", exception);
+            }
+        });
     }
 }
