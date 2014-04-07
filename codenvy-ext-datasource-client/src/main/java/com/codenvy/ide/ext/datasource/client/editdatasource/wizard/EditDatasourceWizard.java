e527bbdcb468ab893bf796ffae599bfe6edb8442
Mickaël Leduque <mickael.leduque@serli.com>
2014-04-07 14:55:48 +0200

    The edit datasource wizard fires 'datasource list changed' events - #21

--- codenvy-ext-datasource-client/src/main/java/com/codenvy/ide/ext/datasource/client/editdatasource/wizard/EditDatasourceWizard.java
+++ codenvy-ext-datasource-client/src/main/java/com/codenvy/ide/ext/datasource/client/editdatasource/wizard/EditDatasourceWizard.java
@@ -17,15 +17,27 @@
 package com.codenvy.ide.ext.datasource.client.editdatasource.wizard;
 
 import com.codenvy.ide.api.notification.NotificationManager;
+import com.codenvy.ide.ext.datasource.client.events.DatasourceListChangeEvent;
 import com.codenvy.ide.ext.datasource.shared.DatabaseConfigurationDTO;
 import com.google.inject.Inject;
 import com.google.inject.assistedinject.Assisted;
+import com.google.web.bindery.event.shared.EventBus;
 
 public class EditDatasourceWizard extends InitializableWizard<DatabaseConfigurationDTO> {
 
+    private final EventBus eventBus;
+
     @Inject
-    public EditDatasourceWizard(NotificationManager notificationManager, @Assisted String title) {
+    public EditDatasourceWizard(NotificationManager notificationManager,
+                                final EventBus eventBus,
+                                @Assisted String title) {
         super(notificationManager, title);
+        this.eventBus = eventBus;
     }
 
+    @Override
+    public void onFinish() {
+        super.onFinish();
+        this.eventBus.fireEvent(new DatasourceListChangeEvent());
+    }
 }
