4cecedeb505320414e64f38a996ebfe66f4553c8
Mickaël Leduque <mickael.leduque@serli.com>
2014-04-07 12:13:27 +0200

    Put connector init in a separate class - #21

---
+++ codenvy-ext-datasource-client/src/main/java/com/codenvy/ide/ext/datasource/client/ConnectorsInitializer.java
@@ -0,0 +1,194 @@
+package com.codenvy.ide.ext.datasource.client;
+
+import static com.codenvy.ide.ext.datasource.client.DatabaseCategoryType.CLOUD;
+import static com.codenvy.ide.ext.datasource.client.DatabaseCategoryType.NOTCLOUD;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.codenvy.ide.collections.Array;
+import com.codenvy.ide.collections.Collections;
+import com.codenvy.ide.ext.datasource.client.newdatasource.connector.AbstractNewDatasourceConnectorPage;
+import com.codenvy.ide.ext.datasource.client.newdatasource.connector.NewDatasourceConnector;
+import com.codenvy.ide.ext.datasource.client.newdatasource.connector.NewDatasourceConnectorAgent;
+import com.codenvy.ide.ext.datasource.client.newdatasource.connector.amazon.ws.mysql.AwsMysqlConnectorPage;
+import com.codenvy.ide.ext.datasource.client.newdatasource.connector.amazon.ws.oracle.AwsOracleConnectorPage;
+import com.codenvy.ide.ext.datasource.client.newdatasource.connector.amazon.ws.postgres.AwsPostgresConnectorPage;
+import com.codenvy.ide.ext.datasource.client.newdatasource.connector.amazon.ws.sqlserver.AwsSqlServerConnectorPage;
+import com.codenvy.ide.ext.datasource.client.newdatasource.connector.google.cloud.sql.GoogleCloudSqlConnectorPage;
+import com.codenvy.ide.ext.datasource.client.newdatasource.connector.mssqlserver.MssqlserverDatasourceConnectorPage;
+import com.codenvy.ide.ext.datasource.client.newdatasource.connector.mysql.MysqlDatasourceConnectorPage;
+import com.codenvy.ide.ext.datasource.client.newdatasource.connector.nuodb.NuoDBDatasourceConnectorPage;
+import com.codenvy.ide.ext.datasource.client.newdatasource.connector.oracle.OracleDatasourceConnectorPage;
+import com.codenvy.ide.ext.datasource.client.newdatasource.connector.postgres.PostgresDatasourceConnectorPage;
+import com.codenvy.ide.util.loging.Log;
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+
+public class ConnectorsInitializer {
+
+    private final List<NewDatasourceConnector> connectors = new ArrayList<>();
+    private final NewDatasourceConnectorAgent  connectorAgent;
+
+    @Inject
+    public ConnectorsInitializer(final NewDatasourceConnectorAgent connectorAgent,
+                                 final DatasourceUiResources resources,
+                                 final Provider<PostgresDatasourceConnectorPage> pgConnectorPageProvider,
+                                 final Provider<MysqlDatasourceConnectorPage> mysqlConnectorPageProvider,
+                                 final Provider<OracleDatasourceConnectorPage> oracleConnectorPageProvider,
+                                 final Provider<MssqlserverDatasourceConnectorPage> mssqlserverConnectorPageProvider,
+                                 final Provider<NuoDBDatasourceConnectorPage> nuodbConnectorPageProvider,
+                                 final Provider<GoogleCloudSqlConnectorPage> googleCloudSqlConnectorPageProvider,
+                                 final Provider<AwsPostgresConnectorPage> awsPostgresConnectorPageProvider,
+                                 final Provider<AwsMysqlConnectorPage> awsMysqlConnectorPageProvider,
+                                 final Provider<AwsOracleConnectorPage> awsOracleConnectorPageProvider,
+                                 final Provider<AwsSqlServerConnectorPage> awsSqlServerConnectorPageProvider) {
+
+        this.connectorAgent = connectorAgent;
+
+        // counter to add different priorities to all connectors - to increment after each #register(NewDatasourceConnector)
+        int connectorCounter = 0;
+
+        // add a new postgres connector
+        Array<Provider< ? extends AbstractNewDatasourceConnectorPage>> pgWizardPages = Collections.createArray();
+        pgWizardPages.add(pgConnectorPageProvider);
+        NewDatasourceConnector connectorPostgres = new NewDatasourceConnector(PostgresDatasourceConnectorPage.PG_DB_ID,
+                                                                              connectorCounter, "PostgreSQL",
+                                                                              resources.getPostgreSqlLogo(),
+                                                                              "org.postgresql.Driver",
+                                                                              pgWizardPages, NOTCLOUD);
+        this.connectors.add(connectorPostgres);
+
+        connectorCounter++;
+
+        // Add a new mysql connector
+        Array<Provider< ? extends AbstractNewDatasourceConnectorPage>> mysqlWizardPages = Collections.createArray();
+        mysqlWizardPages.add(mysqlConnectorPageProvider);
+        NewDatasourceConnector connectorMysql = new NewDatasourceConnector(MysqlDatasourceConnectorPage.MYSQL_DB_ID,
+                                                                           connectorCounter,
+                                                                           "MySQL",
+                                                                           resources.getMySqlLogo(),
+                                                                           "com.mysql.jdbc.Driver",
+                                                                           mysqlWizardPages, NOTCLOUD);
+        this.connectors.add(connectorMysql);
+
+        connectorCounter++;
+
+        // add a new oracle connector
+        Array<Provider< ? extends AbstractNewDatasourceConnectorPage>> oracleWizardPages = Collections.createArray();
+        oracleWizardPages.add(oracleConnectorPageProvider);
+        NewDatasourceConnector connectorOracle = new NewDatasourceConnector(OracleDatasourceConnectorPage.ORACLE_DB_ID,
+                                                                            connectorCounter,
+                                                                            "Oracle",
+                                                                            resources.getOracleLogo(),
+                                                                            "oracle.jdbc.OracleDriver", oracleWizardPages, NOTCLOUD);
+        this.connectors.add(connectorOracle);
+
+        connectorCounter++;
+
+        // add a new SQLserver connector
+        Array<Provider< ? extends AbstractNewDatasourceConnectorPage>> sqlServerWizardPages = Collections.createArray();
+        sqlServerWizardPages.add(mssqlserverConnectorPageProvider);
+        NewDatasourceConnector connectorMs = new NewDatasourceConnector(MssqlserverDatasourceConnectorPage.SQLSERVER_DB_ID,
+                                                                        connectorCounter,
+                                                                        "MsSqlServer",
+                                                                        resources.getSqlServerLogo(),
+                                                                        "net.sourceforge.jtds.jdbc.Driver",
+                                                                        sqlServerWizardPages,
+                                                                        NOTCLOUD);
+        this.connectors.add(connectorMs);
+
+        connectorCounter++;
+
+        // add a new NuoDB connector
+        Array<Provider< ? extends AbstractNewDatasourceConnectorPage>> nuoDBWizardPages = Collections.createArray();
+        nuoDBWizardPages.add(nuodbConnectorPageProvider);
+        NewDatasourceConnector connectorNuoDB = new NewDatasourceConnector(NuoDBDatasourceConnectorPage.NUODB_DB_ID,
+                                                                           connectorCounter,
+                                                                           "NuoDB",
+                                                                           resources.getNuoDBLogo(),
+                                                                           "com.nuodb.jdbc.Driver",
+                                                                           nuoDBWizardPages, NOTCLOUD);
+        this.connectors.add(connectorNuoDB);
+
+        connectorCounter++;
+
+        // add a new GoogleCloudSQL connector
+        Array<Provider< ? extends AbstractNewDatasourceConnectorPage>> googleCloudSQLWizardPages = Collections.createArray();
+        googleCloudSQLWizardPages.add(googleCloudSqlConnectorPageProvider);
+        NewDatasourceConnector connectorGoogle = new NewDatasourceConnector(GoogleCloudSqlConnectorPage.GOOGLECLOUDSQL_DB_ID,
+                                                                            connectorCounter,
+                                                                            "GoogleCloudSQL",
+                                                                            resources.getGoogleCloudSQLLogo(),
+                                                                            "com.mysql.jdbc.Driver",
+                                                                            googleCloudSQLWizardPages,
+                                                                            CLOUD);
+        this.connectors.add(connectorGoogle);
+
+        connectorCounter++;
+
+        // add a new AmazonRDS/Postgres connector
+        Array<Provider< ? extends AbstractNewDatasourceConnectorPage>> awsPostgresWizardPages = Collections.createArray();
+        awsPostgresWizardPages.add(awsPostgresConnectorPageProvider);
+        NewDatasourceConnector connectorAwsPostg = new NewDatasourceConnector(AwsPostgresConnectorPage.AWSPOSTGRES_DB_ID,
+                                                                              connectorCounter,
+                                                                              "Aws/Postgres",
+                                                                              resources.getAwsPostgresLogo(),
+                                                                              "org.postgresql.Driver",
+                                                                              awsPostgresWizardPages,
+                                                                              CLOUD);
+        this.connectors.add(connectorAwsPostg);
+
+        connectorCounter++;
+
+        // add a new AmazonRDS/Mysql connector
+        Array<Provider< ? extends AbstractNewDatasourceConnectorPage>> awsMysqlWizardPages = Collections.createArray();
+        awsMysqlWizardPages.add(awsMysqlConnectorPageProvider);
+        NewDatasourceConnector connectorAwsMySql = new NewDatasourceConnector(AwsMysqlConnectorPage.AWSMYSQL_DB_ID,
+                                                                              connectorCounter,
+                                                                              "Aws/Mysql",
+                                                                              resources.getAwsMysqlLogo(),
+                                                                              "com.mysql.jdbc.Driver",
+                                                                              awsMysqlWizardPages,
+                                                                              CLOUD);
+        this.connectors.add(connectorAwsMySql);
+
+        connectorCounter++;
+
+        // add a new AmazonRDS/Oracle connector
+        Array<Provider< ? extends AbstractNewDatasourceConnectorPage>> awsOracleWizardPages = Collections.createArray();
+        awsOracleWizardPages.add(awsOracleConnectorPageProvider);
+        NewDatasourceConnector connectorAwsOracle = new NewDatasourceConnector(AwsOracleConnectorPage.AWSORACLE_DB_ID,
+                                                                               connectorCounter,
+                                                                               "Aws/Oracle",
+                                                                               resources.getAwsOracleLogo(),
+                                                                               "oracle.jdbc.OracleDriver",
+                                                                               awsOracleWizardPages,
+                                                                               CLOUD);
+        this.connectors.add(connectorAwsOracle);
+
+        connectorCounter++;
+
+        // add a new AmazonRDS/SqlServer connector
+        Array<Provider< ? extends AbstractNewDatasourceConnectorPage>> awsSqlServerWizardPages = Collections.createArray();
+        awsSqlServerWizardPages.add(awsSqlServerConnectorPageProvider);
+        NewDatasourceConnector connectorAwsMs = new NewDatasourceConnector(AwsSqlServerConnectorPage.AWSSQLSERVER_DB_ID,
+                                                                           connectorCounter,
+                                                                           "Aws/SqlServer",
+                                                                           resources.getAwsSqlServerLogo(),
+                                                                           "net.sourceforge.jtds.jdbc.Driver",
+                                                                           awsSqlServerWizardPages,
+                                                                           CLOUD);
+        this.connectors.add(connectorAwsMs);
+
+        connectorCounter++;
+
+        Log.info(ConnectorsInitializer.class, "Connectors registered : " + connectorCounter);
+    }
+
+    public void initConnectors() {
+        for (NewDatasourceConnector connector : this.connectors) {
+            this.connectorAgent.register(connector);
+        }
+    }
+}
--- codenvy-ext-datasource-client/src/main/java/com/codenvy/ide/ext/datasource/client/DatasourceExtension.java
+++ codenvy-ext-datasource-client/src/main/java/com/codenvy/ide/ext/datasource/client/DatasourceExtension.java
@@ -24,26 +24,13 @@ import com.codenvy.ide.api.ui.keybinding.KeyBuilder;
 import com.codenvy.ide.api.ui.wizard.DefaultWizard;
 import com.codenvy.ide.api.ui.workspace.PartStackType;
 import com.codenvy.ide.api.ui.workspace.WorkspaceAgent;
-import com.codenvy.ide.collections.Array;
-import com.codenvy.ide.collections.Collections;
 import com.codenvy.ide.ext.datasource.client.action.EditDatasourcesAction;
 import com.codenvy.ide.ext.datasource.client.common.CellTableResources;
 import com.codenvy.ide.ext.datasource.client.explorer.DatasourceExplorerPartPresenter;
 import com.codenvy.ide.ext.datasource.client.newdatasource.NewDatasourceAction;
 import com.codenvy.ide.ext.datasource.client.newdatasource.NewDatasourceWizardPagePresenter;
 import com.codenvy.ide.ext.datasource.client.newdatasource.NewDatasourceWizardQualifier;
-import com.codenvy.ide.ext.datasource.client.newdatasource.connector.AbstractNewDatasourceConnectorPage;
 import com.codenvy.ide.ext.datasource.client.newdatasource.connector.NewDatasourceConnectorAgent;
-import com.codenvy.ide.ext.datasource.client.newdatasource.connector.amazon.ws.mysql.AwsMysqlConnectorPage;
-import com.codenvy.ide.ext.datasource.client.newdatasource.connector.amazon.ws.oracle.AwsOracleConnectorPage;
-import com.codenvy.ide.ext.datasource.client.newdatasource.connector.amazon.ws.postgres.AwsPostgresConnectorPage;
-import com.codenvy.ide.ext.datasource.client.newdatasource.connector.amazon.ws.sqlserver.AwsSqlServerConnectorPage;
-import com.codenvy.ide.ext.datasource.client.newdatasource.connector.google.cloud.sql.GoogleCloudSqlConnectorPage;
-import com.codenvy.ide.ext.datasource.client.newdatasource.connector.mssqlserver.MssqlserverDatasourceConnectorPage;
-import com.codenvy.ide.ext.datasource.client.newdatasource.connector.mysql.MysqlDatasourceConnectorPage;
-import com.codenvy.ide.ext.datasource.client.newdatasource.connector.nuodb.NuoDBDatasourceConnectorPage;
-import com.codenvy.ide.ext.datasource.client.newdatasource.connector.oracle.OracleDatasourceConnectorPage;
-import com.codenvy.ide.ext.datasource.client.newdatasource.connector.postgres.PostgresDatasourceConnectorPage;
 import com.codenvy.ide.ext.datasource.client.sqllauncher.ExecuteSqlAction;
 import com.codenvy.ide.util.input.CharCodeWithModifiers;
 import com.codenvy.ide.util.input.KeyCodeMap;
@@ -76,19 +63,10 @@ public class DatasourceExtension {
                                NewDatasourceAction newDSConnectionAction,
                                Provider<NewDatasourceWizardPagePresenter> newDatasourcePageProvider,
                                @NewDatasourceWizardQualifier DefaultWizard wizard,
+                               ConnectorsInitializer connectorsInitializer,
                                NewDatasourceConnectorAgent connectorAgent,
                                DatasourceUiResources resources,
                                CellTableResources celltableResources,
-                               Provider<PostgresDatasourceConnectorPage> pgConnectorPageProvider,
-                               Provider<MysqlDatasourceConnectorPage> mysqlConnectorPageProvider,
-                               Provider<OracleDatasourceConnectorPage> oracleConnectorPageProvider,
-                               Provider<MssqlserverDatasourceConnectorPage> mssqlserverConnectorPageProvider,
-                               Provider<NuoDBDatasourceConnectorPage> nuodbConnectorPageProvider,
-                               Provider<GoogleCloudSqlConnectorPage> googleCloudSqlConnectorPageProvider,
-                               Provider<AwsPostgresConnectorPage> awsPostgresConnectorPageProvider ,
-                               Provider<AwsMysqlConnectorPage> awsMysqlConnectorPageProvider,
-                               Provider<AwsOracleConnectorPage> awsOracleConnectorPageProvider,
-                               Provider<AwsSqlServerConnectorPage> awsSqlServerConnectorPageProvider,
                                AvailableJdbcDriversService availableJdbcDrivers,
                                ExecuteSqlAction executeSqlAction,
                                EditDatasourcesAction editDatasourcesAction,
@@ -109,6 +87,9 @@ public class DatasourceExtension {
 
         wizard.addPage(newDatasourcePageProvider);
 
+        // do after adding new datasource page provider to keep page order
+        connectorsInitializer.initConnectors();
+
         // add submenu "Edit datasource" to Datasource menu
         actionManager.registerAction("EditDSConnections", editDatasourcesAction);
         defaultDatasourceMainGroup.add(editDatasourcesAction);
@@ -119,90 +100,6 @@ public class DatasourceExtension {
         // inject CSS
         resources.datasourceUiCSS().ensureInjected();
         celltableResources.cellTableStyle().ensureInjected();
-
-        // counter to add different priorities to all connectors - to increment after each #register(NewDatasourceConnector)
-        int connectorCounter = 0;
-
-        // add a new postgres connector
-        Array<Provider< ? extends AbstractNewDatasourceConnectorPage>> pgWizardPages = Collections.createArray();
-        pgWizardPages.add(pgConnectorPageProvider);
-        connectorAgent.register(PostgresDatasourceConnectorPage.PG_DB_ID, connectorCounter, "PostgreSQL",
-                                resources.getPostgreSqlLogo(), "org.postgresql.Driver", pgWizardPages, NOTCLOUD);
-
-        connectorCounter++;
-
-        // Add a new mysql connector
-        Array<Provider< ? extends AbstractNewDatasourceConnectorPage>> mysqlWizardPages = Collections.createArray();
-        mysqlWizardPages.add(mysqlConnectorPageProvider);
-        connectorAgent.register(MysqlDatasourceConnectorPage.MYSQL_DB_ID, connectorCounter, "MySQL",
-                                resources.getMySqlLogo(), "com.mysql.jdbc.Driver", mysqlWizardPages, NOTCLOUD);
-
-        connectorCounter++;
-
-        // add a new oracle connector
-        Array<Provider< ? extends AbstractNewDatasourceConnectorPage>> oracleWizardPages = Collections.createArray();
-        oracleWizardPages.add(oracleConnectorPageProvider);
-        connectorAgent.register(OracleDatasourceConnectorPage.ORACLE_DB_ID, connectorCounter,
-                                "Oracle", resources.getOracleLogo(), "oracle.jdbc.OracleDriver", oracleWizardPages, NOTCLOUD);
-
-        connectorCounter++;
-
-        // add a new SQLserver connector
-        Array<Provider< ? extends AbstractNewDatasourceConnectorPage>> sqlServerWizardPages = Collections.createArray();
-        sqlServerWizardPages.add(mssqlserverConnectorPageProvider);
-        connectorAgent.register(MssqlserverDatasourceConnectorPage.SQLSERVER_DB_ID, connectorCounter,
-                                "MsSqlServer", resources.getSqlServerLogo(), "net.sourceforge.jtds.jdbc.Driver", sqlServerWizardPages,
-                                NOTCLOUD);
-
-        connectorCounter++;
-
-        // add a new NuoDB connector
-        Array<Provider< ? extends AbstractNewDatasourceConnectorPage>> nuoDBWizardPages = Collections.createArray();
-        nuoDBWizardPages.add(nuodbConnectorPageProvider);
-        connectorAgent.register(NuoDBDatasourceConnectorPage.NUODB_DB_ID, connectorCounter,
-                                "NuoDB", resources.getNuoDBLogo(), "com.nuodb.jdbc.Driver", nuoDBWizardPages, NOTCLOUD);
-        connectorCounter++;
-
-        // add a new GoogleCloudSQL connector
-        Array<Provider< ? extends AbstractNewDatasourceConnectorPage>> googleCloudSQLWizardPages = Collections.createArray();
-        googleCloudSQLWizardPages.add(googleCloudSqlConnectorPageProvider);
-        connectorAgent.register(GoogleCloudSqlConnectorPage.GOOGLECLOUDSQL_DB_ID, connectorCounter,
-                "GoogleCloudSQL", resources.getGoogleCloudSQLLogo(), "com.mysql.jdbc.Driver", googleCloudSQLWizardPages, CLOUD);
-
-        connectorCounter++;
-
-        // add a new AmazonRDS/Postgres connector
-        Array<Provider< ? extends AbstractNewDatasourceConnectorPage>> awsPostgresWizardPages = Collections.createArray();
-        awsPostgresWizardPages.add(awsPostgresConnectorPageProvider);
-        connectorAgent.register(AwsPostgresConnectorPage.AWSPOSTGRES_DB_ID, connectorCounter,
-                "Aws/Postgres", resources.getAwsPostgresLogo(), "org.postgresql.Driver", awsPostgresWizardPages, CLOUD);
-
-        connectorCounter++;
-
-        // add a new AmazonRDS/Mysql connector
-        Array<Provider< ? extends AbstractNewDatasourceConnectorPage>> awsMysqlWizardPages = Collections.createArray();
-        awsMysqlWizardPages.add(awsMysqlConnectorPageProvider);
-        connectorAgent.register(AwsMysqlConnectorPage.AWSMYSQL_DB_ID, connectorCounter,
-                "Aws/Mysql", resources.getAwsMysqlLogo(), "com.mysql.jdbc.Driver", awsMysqlWizardPages, CLOUD);
-
-        connectorCounter++;
-
-        // add a new AmazonRDS/Oracle connector
-        Array<Provider< ? extends AbstractNewDatasourceConnectorPage>> awsOracleWizardPages = Collections.createArray();
-        awsOracleWizardPages.add(awsOracleConnectorPageProvider);
-        connectorAgent.register(AwsOracleConnectorPage.AWSORACLE_DB_ID, connectorCounter,
-                "Aws/Oracle", resources.getAwsOracleLogo(), "oracle.jdbc.OracleDriver", awsOracleWizardPages, CLOUD);
-
-        connectorCounter++;
-
-        // add a new AmazonRDS/SqlServer connector
-        Array<Provider< ? extends AbstractNewDatasourceConnectorPage>> awsSqlServerWizardPages = Collections.createArray();
-        awsSqlServerWizardPages.add(awsSqlServerConnectorPageProvider);
-        connectorAgent.register(AwsSqlServerConnectorPage.AWSSQLSERVER_DB_ID, connectorCounter,
-                "Aws/SqlServer", resources.getAwsSqlServerLogo(), "net.sourceforge.jtds.jdbc.Driver", awsSqlServerWizardPages, CLOUD);
-
-        connectorCounter++;
-
 
         // Add execute shortcut
         actionManager.registerAction(DS_ACTION_SHORTCUT_EXECUTE, executeSqlAction);
