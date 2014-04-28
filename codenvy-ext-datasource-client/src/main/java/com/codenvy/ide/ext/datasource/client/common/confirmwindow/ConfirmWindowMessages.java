df1e1578b08d12ec67570cf1cc226cfb554a4b1c
Mickaël Leduque <mickael.leduque@serli.com>
2014-04-28 16:05:29 +0200

    Add copyright notices - #144

--- codenvy-ext-datasource-client/src/main/java/com/codenvy/ide/ext/datasource/client/common/confirmwindow/ConfirmWindowMessages.java
+++ codenvy-ext-datasource-client/src/main/java/com/codenvy/ide/ext/datasource/client/common/confirmwindow/ConfirmWindowMessages.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.codenvy.ide.ext.datasource.client.common.confirmwindow;
 
 import com.google.gwt.i18n.client.Messages;
