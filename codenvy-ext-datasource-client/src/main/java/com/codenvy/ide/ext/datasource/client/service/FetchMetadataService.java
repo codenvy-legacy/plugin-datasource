e5cb1817749f5d30b5e26c2195c54da73a355cf1
Mickaël Leduque <mickael.leduque@serli.com>
2014-05-16 18:46:56 +0200

    Add missing service javadoc

--- codenvy-ext-datasource-client/src/main/java/com/codenvy/ide/ext/datasource/client/service/FetchMetadataService.java
+++ codenvy-ext-datasource-client/src/main/java/com/codenvy/ide/ext/datasource/client/service/FetchMetadataService.java
@@ -5,9 +5,25 @@ import javax.validation.constraints.NotNull;
 import com.codenvy.ide.ext.datasource.shared.DatabaseConfigurationDTO;
 import com.codenvy.ide.ext.datasource.shared.ExploreTableType;
 
+/**
+ * Service interface for datasource exploration.
+ * 
+ * @author "Mickaël Leduque"
+ */
 public interface FetchMetadataService {
 
+    /***
+     * Explore the given datasource, fetch the metadata, with ExploreTableType.STANDARD category.
+     * 
+     * @param configuration the datasource configuration
+     */
     void fetchDatabaseInfo(@NotNull DatabaseConfigurationDTO configuration);
 
+    /***
+     * Explore the given datasource, fetch the metadata.
+     * 
+     * @param configuration the datasource configuration
+     * @param type the table category
+     */
     void fetchDatabaseInfo(@NotNull DatabaseConfigurationDTO configuration, ExploreTableType type);
 }
