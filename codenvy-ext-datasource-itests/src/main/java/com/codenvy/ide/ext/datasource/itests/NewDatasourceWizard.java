55b50d1ad42ba029f882f31e63b4563e968fa1fd
Stéphane Tournié <stephane.tournie@serli.com>
2014-07-10 14:47:05 +0200

    adapting new datasource wizard webdriver/concordion integration tests

--- codenvy-ext-datasource-itests/src/main/java/com/codenvy/ide/ext/datasource/itests/NewDatasourceWizard.java
+++ codenvy-ext-datasource-itests/src/main/java/com/codenvy/ide/ext/datasource/itests/NewDatasourceWizard.java
@@ -10,15 +10,13 @@
  *******************************************************************************/
 package com.codenvy.ide.ext.datasource.itests;
 
-import com.codenvy.test.framework.AbstractIntegrationTest;
-import com.codenvy.test.framework.selenium.pages.IDEMainPage;
-
-import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-import static org.junit.Assert.fail;
+import com.codenvy.test.framework.AbstractIntegrationTest;
+import com.codenvy.test.framework.selenium.pages.IDEMainPage;
 
 /**
  * Test datasource creation. See specs reports in src/main/resources/com/codenvy/ide/ext/datasource/itests/NewDatasourceWizard.html
@@ -57,11 +55,80 @@ public class NewDatasourceWizard extends AbstractIntegrationTest {
         return newDatasourceWizard.getWizardTitle();
     }
 
+    public String hostedDatabaseCategoryIsAvailable() {
+        if (newDatasourceWizard.isDatasourceCategoryAvailable("hosted_database")) {
+            return "is present";
+        }
+        return "is not present";
+    }
+
+    public String googleCategoryIsAvailable() {
+        if (newDatasourceWizard.isDatasourceCategoryAvailable("google")) {
+            return "is present";
+        }
+        return "is not present";
+    }
+
+    public String amazonCategoryIsAvailable() {
+        if (newDatasourceWizard.isDatasourceCategoryAvailable("amazon")) {
+            return "is present";
+        }
+        return "is not present";
+    }
+    
+    public String hostedDatabaseCategoryTextIsCorrect() {
+        WebElement category = newDatasourceWizard.getHostedDatabaseCategoryElement();
+        if (category.getText().equals("HOSTED DATABASE")) {
+            return "correctly named";
+        }
+        return "badly named";
+    }
+    
+    public String googleCategoryTextIsCorrect() {
+        WebElement category = newDatasourceWizard.getGoogleCategoryElement();
+        if (category.getText().equals("GOOGLE")) {
+            return "correctly named";
+        }
+        return "badly named";
+    }
+    
+    public String amazonCategoryTextIsCorrect() {
+        WebElement category = newDatasourceWizard.getAmazonCategoryElement();
+        if (category.getText().equals("AMAZON")) {
+            return "correctly named";
+        }
+        return "badly named";
+    }
+
+    public String clickOnHostedDatabaseCategory() {
+        WebElement category = newDatasourceWizard.getHostedDatabaseCategoryElement();
+        Actions action = new Actions(driver);
+        action.doubleClick(category);
+        action.perform();
+        return "unfolds";
+    }
+
+    public String clickOnGoogleCategory() {
+        WebElement category = newDatasourceWizard.getGoogleCategoryElement();
+        Actions action = new Actions(driver);
+        action.doubleClick(category);
+        action.perform();
+        return "unfolds";
+    }
+
+    public String clickOnAmazonCategory() {
+        WebElement category = newDatasourceWizard.getAmazonCategoryElement();
+        Actions action = new Actions(driver);
+        action.doubleClick(category);
+        action.perform();
+        return "unfolds";
+    }
+
     public String postgresDsTypeIsAvailable() {
         if (newDatasourceWizard.isDatasourceTypeAvailable("postgres")) {
             return "is enabled";
         }
-        return "is not enabled";
+        return "is disabled";
     }
 
 
@@ -69,26 +136,21 @@ public class NewDatasourceWizard extends AbstractIntegrationTest {
         if (newDatasourceWizard.isDatasourceTypeAvailable("mysql")) {
             return "is enabled";
         }
-        return "is not enabled";
+        return "is disabled";
     }
 
     public String msSQLServerDsTypeIsAvailable() {
         if (newDatasourceWizard.isDatasourceTypeAvailable("sqlserver")) {
             return "is enabled";
         }
-        return "is not enabled";
+        return "is disabled";
     }
 
     public String oracleDsTypeIsNotAvailable() {
-        try {
-            new WebDriverWait(driver, 3).until(gwtToogleButtonToBeEnable(
-                                                                  By.cssSelector("#gwt-debug-datasource-wizard-ds-type-oracle")
-                                                                  ));
-            fail("Oracle shouldn't be enabled by default");
-        } catch (Exception e) {
-            // success
+        if (newDatasourceWizard.isDatasourceTypeNotAvailable("oracle")) {
+            return "is disabled";
         }
-        return "is disabled";
+        return "is enabled";
     }
 
 }
