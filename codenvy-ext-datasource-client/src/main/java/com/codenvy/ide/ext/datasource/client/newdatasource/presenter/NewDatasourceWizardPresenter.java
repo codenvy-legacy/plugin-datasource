f4d147258437179308bccc72ae3e9523d2d11f9d
Stéphane Tournié <stephane.tournie@serli.com>
2014-07-17 15:02:40 +0200

    make space character available for datasource name

--- codenvy-ext-datasource-client/src/main/java/com/codenvy/ide/ext/datasource/client/newdatasource/presenter/NewDatasourceWizardPresenter.java
+++ codenvy-ext-datasource-client/src/main/java/com/codenvy/ide/ext/datasource/client/newdatasource/presenter/NewDatasourceWizardPresenter.java
@@ -113,7 +113,7 @@ public class NewDatasourceWizardPresenter implements WizardDialog, Initializable
 
     @Override
     public void datasourceNameChanged(String name) {
-        RegExp regExp = RegExp.compile("^[A-Za-z0-9_ ]*$");
+        RegExp regExp = RegExp.compile("^[A-Za-z0-9_][A-Za-z0-9_ ]*$");
         if (regExp.test(name)) {
             wizardContext.putData(NewDatasourceWizard.DATASOURCE_NAME, name);
             view.removeNameError();
@@ -136,6 +136,8 @@ public class NewDatasourceWizardPresenter implements WizardDialog, Initializable
             view.setName(datasourceID);
             datasourceNameChanged(datasourceID);
             scheduleInit(categoriesListPage);
+        } else {
+            updateButtonsState();
         }
     }
 
