81cbce9b698ba0cc8de08c64fbf4425dde7b925b
Vitaly Parfonov <vparfonov@codenvy.com>
2015-03-06 18:35:59 +0200

    Merge runner plugin branch

--- codenvy-ext-datasource-client/src/main/java/com/codenvy/ide/ext/datasource/client/discovery/DatasourceDiscovery.java
+++ codenvy-ext-datasource-client/src/main/java/com/codenvy/ide/ext/datasource/client/discovery/DatasourceDiscovery.java
@@ -10,12 +10,10 @@
  *******************************************************************************/
 package com.codenvy.ide.ext.datasource.client.discovery;
 
-import javax.inject.Inject;
-import javax.inject.Singleton;
-
 import com.codenvy.api.runner.dto.ApplicationProcessDescriptor;
 import com.codenvy.api.runner.dto.PortMapping;
 import com.codenvy.ide.api.app.AppContext;
+import com.codenvy.ide.api.app.CurrentProject;
 import com.codenvy.ide.dto.DtoFactory;
 import com.codenvy.ide.ext.datasource.client.DatasourceClientService;
 import com.codenvy.ide.ext.datasource.client.events.DatasourceListChangeEvent;
@@ -23,13 +21,18 @@ import com.codenvy.ide.ext.datasource.client.store.DatasourceManager;
 import com.codenvy.ide.ext.datasource.shared.DatabaseConfigurationDTO;
 import com.codenvy.ide.ext.datasource.shared.DatabaseType;
 import com.codenvy.ide.ext.datasource.shared.TextDTO;
-import com.codenvy.ide.extension.runner.client.run.event.RunnerApplicationStatusEvent;
-import com.codenvy.ide.extension.runner.client.run.event.RunnerApplicationStatusEventHandler;
+import com.codenvy.ide.ext.runner.client.models.Runner;
+import com.codenvy.ide.ext.runner.client.runneractions.impl.launch.common.RunnerApplicationStatusEvent;
+import com.codenvy.ide.ext.runner.client.runneractions.impl.launch.common.RunnerApplicationStatusEventHandler;
 import com.codenvy.ide.rest.AsyncRequestCallback;
 import com.codenvy.ide.rest.StringUnmarshaller;
 import com.codenvy.ide.util.loging.Log;
 import com.google.gwt.http.client.RequestException;
 import com.google.web.bindery.event.shared.EventBus;
+
+import javax.annotation.Nonnull;
+import javax.inject.Inject;
+import javax.inject.Singleton;
 
 /**
  * Eager singleton service that looks at runners status changes and create/remove datasource according to exposed port. If it detects a
@@ -39,16 +42,19 @@ import com.google.web.bindery.event.shared.EventBus;
 public class DatasourceDiscovery {
 
     protected EventBus                eventBus;
+    protected AppContext              appContext;
     protected DatasourceManager       dsManager;
     protected DtoFactory              dtoFactory;
     protected DatasourceClientService dsClientService;
 
     @Inject
     public DatasourceDiscovery(EventBus eventBus,
+                               AppContext appContext,
                                DatasourceManager dsManager,
                                DtoFactory dtoFactory,
                                DatasourceClientService dsClientService) {
         this.eventBus = eventBus;
+        this.appContext = appContext;
         this.dsManager = dsManager;
         this.dtoFactory = dtoFactory;
         this.dsClientService = dsClientService;
@@ -59,49 +65,49 @@ public class DatasourceDiscovery {
      * Add handlers to listen to runners status. Automatically discover and create datasource.
      */
     protected void configureProcessRunEventHandler() {
-        // listen app launch to populate
         eventBus.addHandler(RunnerApplicationStatusEvent.TYPE, new RunnerApplicationStatusEventHandler() {
             @Override
-            public void onRunnerAppRunning(ApplicationProcessDescriptor applicationProcessDescriptor, AppContext appContext) {
-                PortMapping portMapping = applicationProcessDescriptor.getPortMapping();
-                if (portMapping == null) {
+            public void onRunnerStatusChanged(@Nonnull Runner runner) {
+                ApplicationProcessDescriptor descriptor = runner.getDescriptor();
+                if (descriptor == null) {
                     return;
                 }
-                DatabaseConfigurationDTO dsConfig = dtoFactory.createDto(DatabaseConfigurationDTO.class);
-                String projectName = appContext.getCurrentProject().getProjectDescription().getName();
-                long processId = applicationProcessDescriptor.getProcessId();
-                String host = portMapping.getHost();
-                for (DatabaseType databaseType : DatabaseType.values()) {
-                    if (DatabaseType.GOOGLECLOUDSQL.equals(databaseType)) {
-                        // ignore google sql for now as it has the same port then mysql. Maybe clean up and remove it from the list
-                        continue;
-                    }
-                    String mappedPort = portMapping.getPorts().get(Integer.toString(databaseType.getDefaultPort()));
-                    if (mappedPort != null) {
-                        addDatasource(processId, dsConfig, databaseType, projectName, Integer.parseInt(mappedPort), host);
-                    }
+                switch (descriptor.getStatus()) {
+                    case RUNNING:
+                        addDatasource(descriptor);
+                        break;
+                    case FAILED:
+                    case STOPPED:
+                    case CANCELLED:
+                        removeDatasource(descriptor, appContext);
+                        break;
+
+                    default:
                 }
             }
-
-            @Override
-            public void onRunnerAppFailed(ApplicationProcessDescriptor applicationProcessDescriptor, AppContext appContext) {
-                removeDatasource(applicationProcessDescriptor, appContext);
-            }
-
-            @Override
-            public void onRunnerAppStopped(ApplicationProcessDescriptor applicationProcessDescriptor, AppContext appContext) {
-                removeDatasource(applicationProcessDescriptor, appContext);
-            }
-
-            @Override
-            public void onRunnerCancelled(ApplicationProcessDescriptor applicationProcessDescriptor, AppContext appContext) {
-                removeDatasource(applicationProcessDescriptor, appContext);
-            }
-
-            @Override
-            public void onRunnerAppNew(ApplicationProcessDescriptor applicationProcessDescriptor, AppContext appContext) {
-            }
         });
+    }
+
+    private void addDatasource(@Nonnull ApplicationProcessDescriptor applicationProcessDescriptor) {
+        PortMapping portMapping = applicationProcessDescriptor.getPortMapping();
+        CurrentProject currentProject = appContext.getCurrentProject();
+        if (portMapping == null || currentProject == null) {
+            return;
+        }
+        DatabaseConfigurationDTO dsConfig = dtoFactory.createDto(DatabaseConfigurationDTO.class);
+        String projectName = appContext.getCurrentProject().getProjectDescription().getName();
+        long processId = applicationProcessDescriptor.getProcessId();
+        String host = portMapping.getHost();
+        for (DatabaseType databaseType : DatabaseType.values()) {
+            if (DatabaseType.GOOGLECLOUDSQL.equals(databaseType)) {
+                // ignore google sql for now as it has the same port then mysql. Maybe clean up and remove it from the list
+                continue;
+            }
+            String mappedPort = portMapping.getPorts().get(Integer.toString(databaseType.getDefaultPort()));
+            if (mappedPort != null) {
+                addDatasource(processId, dsConfig, databaseType, projectName, Integer.parseInt(mappedPort), host);
+            }
+        }
     }
 
     protected void addDatasource(long processId, final DatabaseConfigurationDTO dsConfig,
@@ -119,20 +125,20 @@ public class DatasourceDiscovery {
         dsConfig.setDatabaseName("");
         try {
             dsClientService.encryptText(databaseType.getDefaultPassword()
-                                        , new AsyncRequestCallback<String>(new StringUnmarshaller()) {
-                                            @Override
-                                            protected void onSuccess(final String encryptedText) {
-                                                TextDTO encryptedTextDTO = dtoFactory.createDtoFromJson(encryptedText, TextDTO.class);
-                                                dsConfig.setPassword(encryptedTextDTO.getValue());
-                                                dsManager.add(dsConfig);
-                                                eventBus.fireEvent(new DatasourceListChangeEvent());
-                                            }
+                    , new AsyncRequestCallback<String>(new StringUnmarshaller()) {
+                @Override
+                protected void onSuccess(final String encryptedText) {
+                    TextDTO encryptedTextDTO = dtoFactory.createDtoFromJson(encryptedText, TextDTO.class);
+                    dsConfig.setPassword(encryptedTextDTO.getValue());
+                    dsManager.add(dsConfig);
+                    eventBus.fireEvent(new DatasourceListChangeEvent());
+                }
 
-                                            @Override
-                                            protected void onFailure(Throwable exception) {
-                                                Log.error(DatasourceDiscovery.class, exception);
-                                            }
-                                        });
+                @Override
+                protected void onFailure(Throwable exception) {
+                    Log.error(DatasourceDiscovery.class, exception);
+                }
+            });
         } catch (RequestException e2) {
             Log.error(DatasourceDiscovery.class, e2);
         }
